a280d42fc5c86987671d630b316baad12a3b905f
EmeraldAmbre <ambresihdom@outlook.fr>
2024-10-10 13:48:57 +0200

    Dawnbridge system

---
+++ Assets/Scripts/World/DrawbridgeSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawbridgeSystem : MonoBehaviour {
+
+    public bool m_isMovingUp;
+    public bool m_isMovingDown;
+
+    [SerializeField] float _minPivot = 0f;
+    [SerializeField] float _maxPivot = 75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
