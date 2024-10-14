dbc33d15e953b4c9d30d80c956101599562648b8
EmeraldAmbre <ambresihdom@outlook.fr>
2024-10-14 17:02:12 +0200

    Spikes System

---
+++ Assets/Scripts/World/Spikes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+    GameObject _player;
+    PlayerManager _manager;
+    void Start() {
+        _player = GameObject.FindWithTag("Player");
+        _manager = _player.GetComponent<PlayerManager>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        _manager.TakeDamage();
+    }
+}
