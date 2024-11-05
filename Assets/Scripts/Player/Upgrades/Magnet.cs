0d3dae41a8c4bd0a0957315a161d11725c7768c9
EmeraldAmbre <ambresihdom@outlook.fr>
2024-11-05 11:25:24 +0100

    UI in prefabs
    Dash text + script
    Magnet text + script

--- Assets/Scripts/Player/Upgrades/Magnet.cs
+++ Assets/Scripts/Player/Upgrades/Magnet.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Magnet : MonoBehaviour {
 
     static int _id = 2;
     static string _name = "magnet";
 
+    PlayerInputAction _input;
+
+    [SerializeField] GameObject _textBox;
+
     public int ID { get { return _id; } private set { _id = value; } }
     public string Name { get { return _name; } private set { _name = value; } }
+
+    void InitInput() {
+        _input = new();
+        _input.Player.CloseBoxText.performed += OnPerformCloseText;
+        _input.Enable();
+    }
+
+    void Start() {
+        _textBox.SetActive(false);
+        InitInput();
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             PlayerPrefs.SetInt(_name, 1);
             PlayerPrefs.Save();
-            gameObject.SetActive(false);
+            _textBox.SetActive(true);
         }
+    }
+
+    void OnDestroy() {
+        _input.Player.CloseBoxText.performed -= OnPerformCloseText;
+        _input.Player.Disable();
+    }
+
+    void OnPerformCloseText(InputAction.CallbackContext context) {
+        _textBox.SetActive(false);
+        gameObject.SetActive(false);
     }
 }
