153d700d0f52409c670071fd801f2e7cef4b7d3d
Xinart <martin.beaussart@gmail.com>
2024-11-05 17:11:03 +0100

    Finishing room 3

--- Assets/Scripts/Tools/GizmoDrawer.cs
+++ Assets/Scripts/Tools/GizmoDrawer.cs
@@ -21,4 +21,23 @@ static public class GizmoDrawer
             lastPoint = nextPoint;
         }
     }
+
+    public static void DrawBox(Vector3 center, Vector3 size)
+    {
+        Gizmos.color = Color.yellow;
+
+        Vector2 halfSize = size / 2;
+
+        // Define corners
+        Vector2 topLeft = (Vector2) center + new Vector2(-halfSize.x, halfSize.y);
+        Vector2 topRight = (Vector2) center + new Vector2(halfSize.x, halfSize.y);
+        Vector2 bottomRight = (Vector2) center + new Vector2(halfSize.x, -halfSize.y);
+        Vector2 bottomLeft = (Vector2) center + new Vector2(-halfSize.x, -halfSize.y);
+
+        // Draw box lines
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, topLeft);
+    }
 }
